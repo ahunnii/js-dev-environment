264ce9fbe667c4658e658d7ea1df09a267775c17
Andrew Hunn <ahunn59@gmail.com>
2018-09-23 11:43:56 -0400

    changes to test

--- src/index.test.js
+++ src/index.test.js
@@ -10,14 +10,14 @@ describe ('Our first test', () => {
 });
 
 describe('index.html', () => {
-  it("should say 'Hello world!'", () => {
+  it("should say 'Hello world!'", (done) => {
       // read file content to variable
       const index = fs.readFileSync('./src/index.html', "utf-8");
 
       // pass this variable to jsdom:
-      jsdom.env(index, function(err, window) {
-          const h1 = window.document.getElementByTagName('h1')[0];   // read our h1
-          expect(h1.innerHTML).to.equal("Helloooooooo World!");      //<---- passed
+      require('jsdom/lib/old-api').env(index, function(err, window) {
+          const h1 = window.document.getElementsByTagName('h1')[0];   // read our h1
+          expect(h1.innerHTML).to.equal("Hello World?");      //<---- passed
           done();
           window.close();
       });
